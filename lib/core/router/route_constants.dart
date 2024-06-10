5bf385a9f9cfbe4763c17f25543361cab77c7238
Arul Karim <lucknoexcuse15@gmail.com>
2024-06-10 09:46:56 +0700

    new

--- lib/core/router/route_constants.dart
+++ lib/core/router/route_constants.dart
@@ -5,4 +5,10 @@ class RouteConstants {
   // dashboard page
   static const String root = 'root';
   static const String rootPath = '/:root_tab';
+  // catergory page
+  static const String categories = 'categories';
+  static const String categoriesPath = 'categories';
+  // detail product
+  static const String product = 'product';
+  static const String productPath = 'product';
 }
